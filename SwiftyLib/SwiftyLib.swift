220764fff4f24fc64210ad55d0ae31df159cf79a
Gasan Akniev <gasan-akniev@yandex-team.ru>
2021-04-17 11:39:03 +0300

    Added test cases

---
+++ SwiftyLib/SwiftyLib.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftyLib.swift
+//  SwiftyLib
+//
+//  Created by Gasan Akniev on 17.04.2021.
+//
+
+public final class SwiftyLib {
+    let name = "SwiftyLib"
+
+    public func add(a: Int, b: Int) -> Int {
+        return a + b
+    }
+
+    public func sub(a: Int, b: Int) -> Int {
+        return a - b
+    }
+}
